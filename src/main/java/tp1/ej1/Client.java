28e039efbbc81c8212bcf2185384fd6bc9766838
ljcanales <ljcanales1404@gmail.com>
2022-03-13 15:22:26 -0300

    Update client ej1

--- src/main/java/tp1/ej1/Client.java
+++ src/main/java/tp1/ej1/Client.java
@@ -6,11 +6,13 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import javax.swing.JOptionPane;
 
 public class Client {
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(Constants.REGISTRY_PORT);
         RemoteCalendar calendar = (RemoteCalendar) registry.lookup("calendar");
-        System.out.println(calendar.getDate());
+        String date = calendar.getDate();
+        JOptionPane.showMessageDialog(null, "Date: " + date);
     }
 }
