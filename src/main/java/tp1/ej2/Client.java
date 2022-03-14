d2bd492d35e04b6e33db19f21fb7aaa5c45eb885
ljcanales <ljcanales1404@gmail.com>
2022-03-13 21:21:32 -0300

    Update client ej2

--- src/main/java/tp1/ej2/Client.java
+++ src/main/java/tp1/ej2/Client.java
@@ -2,6 +2,7 @@ package tp1.ej2;
 
 import utils.Constants;
 
+import javax.swing.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -9,8 +10,34 @@ public class Client {
     public static void main(String[] args) throws Exception {
         Registry registry = LocateRegistry.getRegistry(Constants.REGISTRY_PORT);
         RemoteCurrencyConverter converter = (RemoteCurrencyConverter) registry.lookup("currency-converter");
-        System.out.println("1000 ARS is " + converter.arsToUsd(1000) + " USD");
-        System.out.println("1 USD is " + converter.usdToArs(1) + " ARS");
 
+        Object[] options = {RemoteCurrencyConverter.ARS_TO_USD, RemoteCurrencyConverter.USD_TO_ARS};
+        String selectedOption = (String) JOptionPane.showInputDialog(null,"Seleccionar:", "Currency converter",JOptionPane.QUESTION_MESSAGE,null, options, options[0]);
+
+        if(selectedOption == null)
+            return;
+
+        JTextField amountField = new JTextField();
+        Object[] message = {selectedOption.substring(0, 3)+":", amountField};
+        int option = JOptionPane.showConfirmDialog(null, message, selectedOption, JOptionPane.OK_CANCEL_OPTION);
+
+        if(option != JOptionPane.OK_OPTION)
+            return;
+
+        String result;
+        long amount = Long.parseLong(amountField.getText());
+        switch (selectedOption) {
+            case RemoteCurrencyConverter.ARS_TO_USD:
+                long usd = converter.arsToUsd(amount);
+                result = String.format("%d ARS is %d USD", amount, usd);
+                break;
+            case RemoteCurrencyConverter.USD_TO_ARS:
+                long ars = converter.usdToArs(amount);
+                result = String.format("%d USD is %d ARS", amount, ars);
+                break;
+            default:
+                return;
+        }
+        JOptionPane.showMessageDialog(null, result);
     }
 }
--- src/main/java/tp1/ej2/RemoteCurrencyConverter.java
+++ src/main/java/tp1/ej2/RemoteCurrencyConverter.java
@@ -3,6 +3,8 @@ package tp1.ej2;
 import java.rmi.Remote;
 
 public interface RemoteCurrencyConverter extends Remote {
+    String ARS_TO_USD = "ARS to USD";
+    String USD_TO_ARS = "USD to ARS";
     long arsToUsd(long amount) throws Exception;
     long usdToArs(long amount) throws Exception;
 }
