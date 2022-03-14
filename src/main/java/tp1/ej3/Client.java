f5ab93704b4e12b8ad275f0e3003ffe95a7a21de
ljcanales <ljcanales1404@gmail.com>
2022-03-13 22:27:48 -0300

    Update client ej3

--- src/main/java/tp1/ej3/Client.java
+++ src/main/java/tp1/ej3/Client.java
@@ -2,6 +2,7 @@ package tp1.ej3;
 
 import utils.Constants;
 
+import javax.swing.*;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -11,7 +12,44 @@ public class Client {
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(Constants.REGISTRY_PORT);
         RemotePowerCalculator calculator = (RemotePowerCalculator) registry.lookup("power-calculator");
-        System.out.println("The square root of 25 is " + calculator.square(25));
-        System.out.println("5 raised to the power of 3 is " + calculator.pow(5, 3));
+
+        Object[] options = {RemotePowerCalculator.POWER, RemotePowerCalculator.SQRT};
+        String selectedOption = (String) JOptionPane.showInputDialog(null,"Seleccionar:", "Power Calculator",JOptionPane.QUESTION_MESSAGE,null, options, options[0]);
+
+        if(selectedOption == null)
+            return;
+
+        Object[] message;
+        String result;
+        switch (selectedOption) {
+            case RemotePowerCalculator.POWER:
+                JTextField baseField = new JTextField();
+                JTextField exponentField = new JTextField();
+                message = new Object[]{
+                        "Base: ", baseField,
+                        "Exponent: ", exponentField
+                };
+                if(JOptionPane.showConfirmDialog(null, message, selectedOption, JOptionPane.OK_CANCEL_OPTION) != JOptionPane.OK_OPTION)
+                    return;
+                int base = Integer.parseInt(baseField.getText());
+                int exponent = Integer.parseInt(exponentField.getText());
+                long power = calculator.pow(base, exponent);
+                result = String.format("%d raised to the power of %d is %d", base, exponent, power);
+                break;
+            case RemotePowerCalculator.SQRT:
+                JTextField numberField = new JTextField();
+                message = new Object[]{
+                        "Number: ", numberField,
+                };
+                if(JOptionPane.showConfirmDialog(null, message, selectedOption, JOptionPane.OK_CANCEL_OPTION) != JOptionPane.OK_OPTION)
+                    return;
+                int number = Integer.parseInt(numberField.getText());
+                long sqrt = calculator.square(number);
+                result = String.format("The square root of %d is %d", number, sqrt);
+                break;
+            default:
+                return;
+        }
+        JOptionPane.showMessageDialog(null, result);
     }
 }
--- src/main/java/tp1/ej3/RemotePowerCalculator.java
+++ src/main/java/tp1/ej3/RemotePowerCalculator.java
@@ -4,6 +4,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RemotePowerCalculator extends Remote {
+    String POWER = "Power";
+    String SQRT = "Square root";
     long square(int n) throws RemoteException;
     long pow(int n1, int n2) throws RemoteException;
 }
